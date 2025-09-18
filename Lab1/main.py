38c91b10e83b9b1b3165afb25215e5de53f808ff
VavakinV <vladvavakin@gmail.com>
2025-09-19 00:21:44 +0300

    Lab1 v1.0

--- Lab1/main.py
+++ Lab1/main.py
@@ -70,7 +70,7 @@ def task4():
     out.release()
     cv.destroyAllWindows()
 
-
+# Отображение изображение в форматах BGR и HSV
 def task5():
     img = cv.imread("media/chicken.png")
     img_HSV = cv.cvtColor(img, cv.COLOR_BGR2HSV)
@@ -84,4 +84,122 @@ def task5():
     cv.waitKey(0)
     cv.destroyAllWindows()
 
-task5()
+# Вывод в окно изображения с камеры с красным крестом в середине
+def task6():
+    cap = cv.VideoCapture(0)
+    cv.namedWindow("Webcam", cv.WINDOW_AUTOSIZE)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        h, w = frame.shape[:2]
+        cx, cy = w // 2, h // 2
+
+        rect_w, rect_h = 100, 20
+        rect_v_w, rect_v_h = 20, 100
+
+        cv.rectangle(frame, (cx - rect_w // 2, cy - rect_h // 2),
+                            (cx + rect_w // 2, cy + rect_h // 2),
+                            (0, 0, 255), 2)
+        cv.rectangle(frame, (cx - rect_v_w // 2, cy - rect_v_h // 2),
+                            (cx + rect_v_w // 2, cy + rect_v_h // 2),
+                            (0, 0, 255), 2)
+
+        cv.imshow("Webcam", frame)
+
+        if cv.waitKey(1) & 0xFF == 27:
+            break
+
+    cap.release()
+    cv.destroyAllWindows()
+
+# Запись видео с камеры в файл media/web_output.mp4
+def task7():
+    cap = cv.VideoCapture(0)
+    cv.namedWindow("Webcam", cv.WINDOW_AUTOSIZE)
+
+    fps = cap.get(cv.CAP_PROP_FPS)
+    w = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+    h = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+    fourcc = cv.VideoWriter_fourcc(*'mp4v')
+    out = cv.VideoWriter("media/web_output.mp4", fourcc, fps, (w, h))
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        cv.imshow("Webcam", frame)
+        out.write(frame)
+
+        if cv.waitKey(1) & 0xFF == 27:
+            break
+
+    cap.release()
+    out.release()
+    cv.destroyAllWindows()
+
+# Определение цвета центрального пикселя и рисование соответствующего креста
+def task8():
+    cap = cv.VideoCapture(0)
+    cv.namedWindow("Webcam", cv.WINDOW_AUTOSIZE)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+
+        h, w = frame.shape[:2]
+        cx, cy = w // 2, h // 2
+
+        b, g, r = frame[cy, cx]
+        print(frame[cy, cx])
+        if r >= g and r >= b:
+            color = (0, 0, 255)
+        elif g >= r and g >= b:
+            color = (0, 255, 0)
+        else:
+            color = (255, 0, 0)
+
+        rect_w, rect_h = 100, 10
+        rect_v_w, rect_v_h = 10, 100
+
+        cv.rectangle(frame, (cx - rect_w // 2, cy - rect_h // 2),
+                            (cx + rect_w // 2, cy + rect_h // 2),
+                            color, -1)
+
+        cv.rectangle(frame, (cx - rect_v_w // 2, cy - rect_v_h // 2),
+                            (cx + rect_v_w // 2, cy + rect_v_h // 2),
+                            color, -1)
+
+        cv.imshow("Webcam", frame)
+
+        if cv.waitKey(1) & 0xFF == 27:
+            break
+
+    cap.release()
+    cv.destroyAllWindows()
+
+# Отображение видео с камеры телефона (через ivCam)
+def task9():
+    cap = cv.VideoCapture(1)
+    cv.namedWindow("PhoneCam", cv.WINDOW_AUTOSIZE)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        cv.imshow("PhoneCam", frame)
+
+        if cv.waitKey(1) & 0xFF == 27:
+            break
+
+    cap.release()
+cv.destroyAllWindows()
+
+task9()
